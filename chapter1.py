b36a9b59274d5a366b2d6e246c084c8ab60ae5a8
Jeremiah Daniels <jedaniels3933@gmail.com>
2024-08-09 04:00:21 -0400

    1st

---
+++ chapter1.py
@@ -0,0 +1,13 @@
+from helper import d, clear
+from main import current_character
+'''
+Character starts with memory loss. Story begins below, they will eventaully have to remember their name then will create the Character object and then they eventaully come across 3 weapons that wioll decide character class 
+'''
+
+def intro():
+    clear()
+    d()
+    print('''
+You awaken with a start, your breath ragged and cold as it billows into the misty air. The ground beneath you is damp, covered in a thick carpet of decaying leaves, their once vibrant colors long since faded to a lifeless brown. Tall, twisted trees loom above, their gnarled branches clawing at the murky sky like skeletal hands reaching for salvation. A faint glow filters through the dense canopy, casting eerie shadows that dance and flicker as if alive.
+''')
+    d()
